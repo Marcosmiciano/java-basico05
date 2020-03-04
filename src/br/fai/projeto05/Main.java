adbc1a01e2a3de5de5cbe6eb72172f8c68dfd7ee
marcosmiciano <miciano.marcos@gmail.com>
2020-03-04 20:57:31 -0300

    Resolução do problema

--- src/br/fai/projeto05/Main.java
+++ src/br/fai/projeto05/Main.java
@@ -11,6 +11,20 @@ public class Main {
 
 	private void start() {
 		System.out.println("Enviando esta branch para o github");
+		
+		for(int i = 0; i < 5; i++) {
+			switch (i) {
+			case 1:
+				System.out.println("Primeira condição");
+				break;
+			case 4:
+				System.out.println("Segunda condição");
+				break;
+				default:
+					System.out.println("Não encontrou nenhuma condição");
+					break;
+			}
+		}
 	}
 
 }
